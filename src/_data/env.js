184ba1b78246853305b7f8a8c50b3ca4e144a222
Roman Edirisinghe <rediris@users.noreply.github.com>
2025-05-19 12:59:23 -0400

    delete moved configs

--- src/_data/env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  INTERCOM_APP_ID: process.env.INTERCOM_APP_ID || 'setme',
-  GOOGLE_ANALYTICS_4_ID: process.env.GOOGLE_ANALYTICS_4_ID || 'setme',
-  ONETRUST_COOKIE_SCRIPT_ID: process.env.ONETRUST_COOKIE_SCRIPT_ID || 'setme',
-};
